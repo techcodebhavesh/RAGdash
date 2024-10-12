48ac4b0e2d1040502c36e4beaae9ff24c332986d
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2024-10-12 16:11:35 +0530

    csv implementation improved

--- noopurkiscsv.py
+++ noopurkiscsv.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from ragdash.chromadb import ChromaDB_VectorStore
 from ragdash.groq import Groq
-from groq import Groq as qqqq
 import streamlit as st
 from dotenv import load_dotenv
 import os
@@ -32,55 +31,12 @@ rd = MyRAGdash(config=config)
 csv_path = 'F:/Mayur/vit/innov8ors/ollama/AutoDash/Sample_CSVs/dairy_dataset.csv'  # Path to your CSV file
 df_i = pd.read_csv(csv_path)
 
+python_code = rd.generate_python(df=df_i, type_c="pie")
 
-
-client = qqqq(
-    api_key="bvhgkml",
-)
-
-info=df_i.dtypes.astype(str)
-# Create a prompt for the LLM to generate Python code for a DataFrame for a bar chart
-csv_description_prompt = f"""
-I have a CSV file with the following description:
-{info}
-
-Consider the CSV is read before in a DataFrame df_i. Can you generate the Python  code to extract and store dataframe df to make a bar chart? Assume the data is in a pandas dataframe called 'df_i'.Do not plot the graph, just extract the dataframe df and store it. Respond with only Python code. Do not answer with any explanations -- just the code.
-"""
-
-# Send the prompt to the Groq API
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "system", 
-            "content": "You are an python expert. Please help me generate a python code to extract and store dataframe df needed to make a bar chart.Your response should ONLY be based on the given context and follow the response guidelines and format instructions.",
-        },
-        {
-            "role": "user",
-            "content": csv_description_prompt,
-        }
-    ],
-    model="llama3-8b-8192",
-)
-
-# Print the generated Python code for DataFrame extraction
-extracted_df_code = chat_completion.choices[0].message.content
-cleaned_response = extracted_df_code.replace("```", "").strip()
-print("Extracted DataFrame Code:")
-print(cleaned_response)
-dfs = {
-        0: df_i  # Use df read from the CSV
-    }
+df = rd.extract_dataframe(python_code,df=df_i)
 
 dirname = os.path.dirname(__file__)
 
-execution_context = {"pd": pd, "df_i": df_i, **dfs}
-
-exec_context = execution_context.copy()
-exec_context['dirname'] = dirname  # Include dirname in the context
-exec_context.update(dfs)  # Include dfs in the context
-
-exec(cleaned_response,exec_context)
-df = exec_context.get('df', None)
 
 print("df")
 print(df)
@@ -89,7 +45,8 @@ print("df")
 # Prepare a new prompt to get the Plotly code
 
 
-code = rd.generate_plotly_code(question="my_question", sql="sql", df=df,type_c="")
+code = rd.generate_plotly_code(question=None,df_metadata= df.columns,
+                               sql=None, df=df,type_c="pie")
 
 fig = rd.get_plotly_figure(plotly_code=code, df=df)
 st.plotly_chart(fig, use_container_width=True)
